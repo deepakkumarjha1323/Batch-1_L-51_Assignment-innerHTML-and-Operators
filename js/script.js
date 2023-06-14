45e86d751f2a2c6c398221c530a5cecef73f2efe
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-14 13:55:01 +0530

    L-51-InnerHTML

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Arithmetic Operators
+
+var a = 5;
+var b = 7;
+
+console.log(a+b); // Addition Operator
+console.log(b-a); // Subtraction Operator
+console.log(a*b); // Multiplication Operator
+console.log(a/b); // Division Operator
